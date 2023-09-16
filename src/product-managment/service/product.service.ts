40d34936a74f35a8da229e4920f4131c2478549f
XFernando43 <102422211+XFernando43@users.noreply.github.com>
2023-09-16 01:16:57 -0500

    folder distribution fixed for monolitic application

--- src/product-managment/service/product.service.ts
+++ src/product-managment/service/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import {InjectRepository} from '@nestjs/typeorm';
-import { product } from '../entites/product.entity';
+import { product } from '../Domain/entites/product.entity';
 
 @Injectable()
 export class ProductService {
