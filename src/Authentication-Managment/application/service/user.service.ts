abbdc9b474456c97d41e6483113e4e73a048d29a
XFernando43 <U20171E997@upc.edu.pe>
2023-09-24 19:43:33 -0500

    add shoppingCart to user

--- src/Authentication-Managment/application/service/user.service.ts
+++ src/Authentication-Managment/application/service/user.service.ts
@@ -6,12 +6,15 @@ import { UserRequest } from 'src/Authentication-Managment/Domain/request/user.re
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { categoryRequest } from 'src/product-managment/Domain/request/categoryRequest';
+import { ShoppingCart } from 'src/Shopping-Managment/domain/entities/ShoppingCart.entity';
+import { PaymentState } from 'src/Shopping-Managment/domain/enums/paymentState';
 
 @Injectable()
 export class UsersService {
     constructor
     (@InjectRepository(User) private UserRepository:Repository<User>,
-     @InjectRepository(Account) private AccountRepository:Repository<Account>
+     @InjectRepository(Account) private AccountRepository:Repository<Account>,
+    //  @InjectRepository(ShoppingCart) private ShoppingCartRepository:Repository<ShoppingCart>
     ){}
 
     async listUsers(){
@@ -28,8 +31,23 @@ export class UsersService {
             newAccount.password = hashedPassword;
             newAccount.user = newUser;
             
+            //Shopping Cart
+            /* 
+            let newShoppingCart = new ShoppingCart();
+            newShoppingCart.items = null;
+            newShoppingCart.totalPrice = 0;
+            newShoppingCart.paymentDate = null;
+            newShoppingCart.paymentStatus = PaymentState.NOT_ARTICLES;
+            newShoppingCart.userId = newUser;
+            const ShoppingCartToDb = await this.ShoppingCartRepository.create(newShoppingCart);
+            */
+            //
+            
+            
             await this.UserRepository.save(newUser);
             await this.AccountRepository.save(newAccount);        
+            
+            // await this.ShoppingCartRepository.save(ShoppingCartToDb)
             
             return newAccount;
         } catch (error) {
--- src/testing/ItemCart/item-cart.module.ts
+++ src/testing/ItemCart/item-cart.module.ts
@@ -4,9 +4,10 @@ import { ItemCartController } from './item-cart.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CartItems } from 'src/Shopping-Managment/domain/entities/cartItems.entity';
 import { product } from 'src/product-managment/Domain/entities/product.entity';
+import { ShoppingCart } from 'src/Shopping-Managment/domain/entities/ShoppingCart.entity';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([CartItems,product])],
+  imports:[TypeOrmModule.forFeature([CartItems,product,ShoppingCart])],
   providers: [ItemCartService],
   controllers: [ItemCartController]
 })
